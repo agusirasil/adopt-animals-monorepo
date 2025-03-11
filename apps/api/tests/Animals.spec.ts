864fd9086a829638002bf7fb734c57c90c15c3ec
agus-irazusta <ag.irazusta@gmail.com>
2025-03-11 16:44:12 -0300

    Changed variable and file names

--- apps/api/tests/Animals.spec.ts
+++ apps/api/tests/Animals.spec.ts
@@ -1,201 +1,188 @@
 import { test, expect, request, APIRequestContext } from '@playwright/test';
-import { defineConfig } from '@playwright/test';
-export default defineConfig({
-  use: {
-    proxy: {
-      server: 'http://local:8080',
-    },
-  }
-});
-import AnimalsListDataSet from '../utils/AnimalsListTestsData.json'
-import NewPetData from '../utils/NewPetDataSet.json'
-import NewPetDataFail from '../utils/NewPetDataSetFail.json'
-import NewPetDataFailEmpty from '../utils/NewPetDataFailEmpty.json'
-import NewPetDataSetFailWrongParameter from '../utils/NewPetDataSetFailWrongParameter.json'
-import NewPetDataUpdate from '../utils/NewPetDataSetUpdate.json'
+import { API_BASE_URL } from '../config';
 
-let url: string;
+import AnimalsList from './data-sets/AnimalsList.json'
+import NewPet from './data-sets/NewPet.json'
+import NewPetIncorrectData from './data-sets/NewPetIncorrectData.json'
+import NewPetEmptyForm from './data-sets/NewPetEmptyForm.json'
+import NewPetWrongParameter from './data-sets/NewPetWrongParameter.json'
+import NewPetUpdate from './data-sets/NewPetUpdate.json'
+
 let apiContext: APIRequestContext;
+const baseURL = API_BASE_URL;
 
 test.describe('Animals Resource', () => {
   test.describe.configure({ mode: 'serial' });
-
-  //Preparation Test
   test.beforeAll('Preparation', async () => {
     apiContext = await request.newContext();
-    url = "http://localhost:8000/api/v1";
   });
 
   //GET Pet List Pass
   test("GET animals should return the pet list with the correct data", async () => {
-    const PetList = await apiContext.get(`${url}/animals`);
-    const responseBody = await PetList.json();
-    expect(PetList).toBeOK();
-    expect(responseBody).toEqual(AnimalsListDataSet);
+    const petList = await apiContext.get(`${baseURL}/animals`);
+    const petListData = await petList.json();
+    expect(petList).toBeOK();
+    expect(petListData).toEqual(AnimalsList);
   });
 
   //GET Pet List Fail
   test("GET animals using the incorrect resource should return 404", async () => {
-    const PetList = await apiContext.get(`${url}/animalls`);
-    expect(PetList.status()).toBe(404);
+    const petList = await apiContext.get(`${baseURL}/animalls`);
+    expect(petList.status()).toBe(404);
   });
 
   //GET First Pet Pass
   test("GET animal with id:1 should return the first pet of the list with the correct data", async () => {
-    const firstPet = await apiContext.get(`${url}/animals/1`);
-    const responseBody = await firstPet.json();
-    expect(firstPet).toBeOK();
-    const expectedData = AnimalsListDataSet[0];
-    expect(responseBody).toEqual(expectedData);
+    const firstPetResponse = await apiContext.get(`${baseURL}/animals/1`);
+    const firstPetData = await firstPetResponse.json();
+    expect(firstPetResponse).toBeOK();
+    const expectedData = AnimalsList[0];
+    expect(firstPetData).toEqual(expectedData);
   });
 
   //GET First Pet Fails
   test("GET animal with id:1 using the incorrect resource should return 404", async () => {
-    const firstPet = await apiContext.get(`${url}/animalls/1`);
-    expect(firstPet.status()).toBe(404);
+    const firstPetResponse = await apiContext.get(`${baseURL}/animalls/1`);
+    expect(firstPetResponse.status()).toBe(404);
   });
 
   test("GET animal with a non-existent id should return 404", async () => {
-    const firstPet = await apiContext.get(`${url}/animals/45`);
-    expect(firstPet.status()).toBe(404);
+    const firstPetResponse = await apiContext.get(`${baseURL}/animals/45`);
+    expect(firstPetResponse.status()).toBe(404);
   });
 
   test("GET animal with an invalid id should return 400", async () => {
-    const firstPet = await apiContext.get(`${url}/animals/ñ`);
-    expect(firstPet.status()).toBe(400);
+    const firstPetResponse = await apiContext.get(`${baseURL}/animals/ñ`);
+    expect(firstPetResponse.status()).toBe(400);
   });
 
   //POST Add New Pet Pass
-  test("POST usign the correct data should add a new pet", async () => {
-    const postPetRequest = await apiContext.post(`${url}/animals`, {
-      data: NewPetData
+  test("POST using the correct data should add a new pet", async () => {
+    const postPetResponse = await apiContext.post(`${baseURL}/animals`, {
+      data: NewPet
     });
-    expect(postPetRequest).toBeOK();
-    const postPetRequestData = await postPetRequest.json();
+    expect(postPetResponse).toBeOK();
+    const postPetData = await postPetResponse.json();
 
     // GET Request to compare with
-    const getNewPetResponse = await apiContext.get(`${url}/animals/${postPetRequestData.id}`);
-    const getNewPetResponseData = await getNewPetResponse.json();
+    const getPetResponse = await apiContext.get(`${baseURL}/animals/${postPetData.id}`);
+    const getPetResponseData = await getPetResponse.json();
     const expectedData = {
-      ...NewPetData,
-      id: postPetRequestData.id,
-      birthdate: postPetRequestData.birthdate
+      ...NewPet,
+      id: postPetData.id,
+      birthdate: postPetData.birthdate
     }
-    expect(getNewPetResponseData).toEqual(expectedData);
+    expect(getPetResponseData).toEqual(expectedData);
   });
 
   //POST Add New Pet Fails
-  test("POST usign the incorrect data should return 400", async () => {
-    const postPetRequest = await apiContext.post(`${url}/animals`, {
-      data: NewPetDataFail
+  test("POST using the incorrect data should return 400", async () => {
+    const postPetResponse = await apiContext.post(`${baseURL}/animals`, {
+      data: NewPetIncorrectData
     });
-    expect(postPetRequest.status()).toBe(400);
+    expect(postPetResponse.status()).toBe(400);
   });
 
-  test("POST usign invalid parameters should return 400", async () => {
-    const postPetRequest = await apiContext.post(`${url}/animals`, {
-      data: NewPetDataSetFailWrongParameter
+  test("POST using invalid parameters should return 400", async () => {
+    const postPetResponse = await apiContext.post(`${baseURL}/animals`, {
+      data: NewPetWrongParameter
     });
-    expect(postPetRequest.status()).toBe(400);
+    expect(postPetResponse.status()).toBe(400);
   });
 
-  test("POST usign the incorrect resource should return 404", async () => {
-    const postPetRequest = await apiContext.post(`${url}/animalls`, {
-      data: NewPetData
+  test("POST using the incorrect resource should return 404", async () => {
+    const postPetResponse = await apiContext.post(`${baseURL}/animalls`, {
+      data: NewPet
     });
-    expect(postPetRequest.status()).toBe(404);
+    expect(postPetResponse.status()).toBe(404);
   });
 
-  test("POST usign an empty data set should return 400", async () => {
-    const postPetRequest = await apiContext.post(`${url}/animals`, {
-      data: NewPetDataFailEmpty
+  test("POST using an empty data set should return 400", async () => {
+    const postPetResponse = await apiContext.post(`${baseURL}/animals`, {
+      data: NewPetEmptyForm
     });
-    expect(postPetRequest.status()).toBe(400);
+    expect(postPetResponse.status()).toBe(400);
   });
 
   //PUT (Update) Pets Pass
   test("PUT using the correct data should update a pet", async () => {
-    const PutUpdatedPet = await apiContext.put(`${url}/animals/2`,
-      {
-        data: NewPetDataUpdate
-      });
-    expect(PutUpdatedPet.ok()).toBeTruthy();
-    const putPetRequestData = await PutUpdatedPet.json();
+    const updatedPetResponse = await apiContext.put(`${baseURL}/animals/2`, {
+      data: NewPetUpdate
+    });
+    expect(updatedPetResponse.ok()).toBeTruthy();
+    const updatedPetData = await updatedPetResponse.json();
 
     // GET Request to compare with
-    const getNewPetResponse = await apiContext.get(`${url}/animals/${putPetRequestData.id}`);
-    const getNewPetResponseData = await getNewPetResponse.json();
+    const getPetResponse = await apiContext.get(`${baseURL}/animals/${updatedPetData.id}`);
+    const getPetResponseData = await getPetResponse.json();
     const expectedData = {
-      ...NewPetDataUpdate,
-      id: putPetRequestData.id,
-      birthdate: putPetRequestData.birthdate
+      ...NewPetUpdate,
+      id: updatedPetData.id,
+      birthdate: updatedPetData.birthdate
     }
-    expect(getNewPetResponseData).toEqual(expectedData);
+    expect(getPetResponseData).toEqual(expectedData);
   });
 
   //PUT (Update) Pets Fails
   test("PUT using the incorrect resource should return 404", async () => {
-    const PutUpdatedPet = await apiContext.put(`${url}/animalls/2`,
-      {
-        data: NewPetDataUpdate
-      });
-      expect(PutUpdatedPet.status()).toBe(404);
+    const updatedPetResponse = await apiContext.put(`${baseURL}/animalls/2`, {
+      data: NewPetUpdate
+    });
+    expect(updatedPetResponse.status()).toBe(404);
   });
-  
+
   test("PUT using the incorrect data should return 400", async () => {
-    const PutUpdatedPet = await apiContext.put(`${url}/animals/2`,
-      {
-        data: NewPetDataFail
-      });
-      expect(PutUpdatedPet.status()).toBe(400);
+    const updatedPetResponse = await apiContext.put(`${baseURL}/animals/2`, {
+      data: NewPetIncorrectData
+    });
+    expect(updatedPetResponse.status()).toBe(400);
   });
 
   test("PUT using invalid parameters should return 400", async () => {
-    const PutUpdatedPet = await apiContext.put(`${url}/animals/2`,
-      {
-        data: NewPetDataSetFailWrongParameter
-      });
-      expect(PutUpdatedPet.status()).toBe(400);
+    const updatedPetResponse = await apiContext.put(`${baseURL}/animals/2`, {
+      data: NewPetWrongParameter
+    });
+    expect(updatedPetResponse.status()).toBe(400);
   });
 
   test("PUT using an empty data set should return 400", async () => {
-    const PutUpdatedPet = await apiContext.put(`${url}/animals/2`, {
-      data: NewPetDataFailEmpty
+    const updatedPetResponse = await apiContext.put(`${baseURL}/animals/2`, {
+      data: NewPetEmptyForm
     });
-    expect(PutUpdatedPet.status()).toBe(400);
+    expect(updatedPetResponse.status()).toBe(400);
   });
 
   test("PUT using an invalid id should return 400", async () => {
-    const PutUpdatedPet = await apiContext.put(`${url}/animals/ñ`, {
-      data: NewPetDataFailEmpty
+    const updatedPetResponse = await apiContext.put(`${baseURL}/animals/ñ`, {
+      data: NewPetEmptyForm
     });
-    expect(PutUpdatedPet.status()).toBe(400);
+    expect(updatedPetResponse.status()).toBe(400);
   });
 
   //DELETE Pets Pass
   test("DELETE using the correct data should delete a pet", async () => {
-    const deletedPet = await apiContext.delete(`${url}/animals/2`);
-    expect(deletedPet).toBeOK();
-    
+    const deletedPetResponse = await apiContext.delete(`${baseURL}/animals/2`);
+    expect(deletedPetResponse).toBeOK();
+
     //GET Request should be 404
-    const getNewPetResponse = await apiContext.get(`${url}/animals/2`);
-    expect(getNewPetResponse.status()).toBe(404);
+    const getPetResponse = await apiContext.get(`${baseURL}/animals/2`);
+    expect(getPetResponse.status()).toBe(404);
   });
-  
+
   //DELETE Pets Fails
   test("DELETE using the incorrect resource should return 404", async () => {
-    const deletedPet = await apiContext.delete(`${url}/animalls/2`);
-    expect(deletedPet.status()).toBe(404);
+    const deletedPetResponse = await apiContext.delete(`${baseURL}/animalls/2`);
+    expect(deletedPetResponse.status()).toBe(404);
   });
 
   test("DELETE using with a non-existent id should return 404", async () => {
-    const deletedPet = await apiContext.delete(`${url}/animals/45`);
-    expect(deletedPet.status()).toBe(404);
+    const deletedPetResponse = await apiContext.delete(`${baseURL}/animals/45`);
+    expect(deletedPetResponse.status()).toBe(404);
   });
 
   test("DELETE using with an invalid id should return 400", async () => {
-    const deletedPet = await apiContext.delete(`${url}/animals/ñ`);
-    expect(deletedPet.status()).toBe(400);
+    const deletedPetResponse = await apiContext.delete(`${baseURL}/animals/ñ`);
+    expect(deletedPetResponse.status()).toBe(400);
   });
 });
 
